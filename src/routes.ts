11e2d08611ca258ffd2748d7dac51f3d7b0d11a6
imkimura <kimurajulia15@gmail.com>
2021-11-13 16:50:22 -0300

    authors

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 import StudentsController from './controllers/StudentsController';
 
@@ -12,5 +12,33 @@ routes.get('/students/:id', studentsController.show);
 routes.put('/students/:id', studentsController.update);
 routes.delete('/students/:id', studentsController.delete);
 
+routes.get('/', (req: Request, res: Response) => {
+    const response = {
+        'Authors': [
+            {
+                name: 'Julia Kimura Silva',
+                ra: '585890',
+            },
+            {
+                name: 'Elton Tanaka Haruji',
+                ra: '577261',
+            },
+            {
+                name: 'João Victor de Oliveira Andrade',
+                ra: '582141',
+            },
+            {
+                name: 'João Daum',
+                ra: '581941',
+            },
+            {
+                name: 'Wangner Mafra',
+                ra: '583154',
+            }
+        ]
+    };
+
+    return res.json(response).status(200);
+})
 
 export default routes;
